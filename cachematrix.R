9b0e9c47a0d196f00fdbc0d53334a9248c381020
Jiwoo Song <jiwsong@gmail.com>
2015-01-22 16:37:06 -0800

    filled in codes to successfully create a cache matrix and store the inverse value in the environment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL # to initialize the variable
+  set <- function(y) { # sets initial values to inverse variable in the funciton env
+    inv <<- NULL # initialize the inv variable in new environment
+    x <<- y # store matrix x in new environment
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse # when called, sets inverse matrix into inv variable in the env
+  getinverse <- function() inv
+  list(set = set, get = get, 
+       setinverse = setinverse, 
+       getinverse = getinverse)
 }
 
 
@@ -12,4 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse() # recall getinverse funciton within x, set it to variable inv
+  if(!is.null(inv)) { # if inv variable contains value, return 'cached' copy
+    message("getting cached data")
+    return(inv) 
+  }
+  matrix <- x$get()
+  inv <- solve(matrix) # solve for inverse matrix
+  x$setinverse(inv) # recall setinverse function to store in the env.
+  return(inv)
 }
+
